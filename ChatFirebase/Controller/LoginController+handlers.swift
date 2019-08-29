f5df4f3a0a78c708b9ec65101c7f6e9ea8b7a16c
Alejandro <alejandro.rob.alt@gmail.com>
2019-08-28 22:01:46 -0500

    Get image from the picker controller

--- ChatFirebase/Controller/LoginController+handlers.swift
+++ ChatFirebase/Controller/LoginController+handlers.swift
@@ -8,10 +8,36 @@
 
 import UIKit
 
-extension LoginController{
+extension LoginController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
     @objc func handleSelectProfileImageView(){
         let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
         present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let originalImage = info[.originalImage] as? UIImage  {
+            selectedImageFromPicker = originalImage
+        }
+        else if let editedImage = info[.editedImage] as? UIImage {
+            selectedImageFromPicker = editedImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker{
+            profileImageView.image = selectedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true, completion: nil)
     }
 }
 
