6da825ae55f77ce92561edec3ef4fbd4ad3c5bf1
Alejandro <alejandro.rob.alt@gmail.com>
2019-09-03 10:12:02 -0500

    Bubble view view message customized

--- ChatFirebase/View/ChatMessageCell.swift
+++ ChatFirebase/View/ChatMessageCell.swift
@@ -15,18 +15,40 @@ class ChatMessageCell: UICollectionViewCell {
         textView.text = "Sample text for now"
         textView.font = UIFont.systemFont(ofSize: 16)
         textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .clear
+        textView.textColor = .white
         return textView
     }()
+    
+    let bubbleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 0, g: 137, b: 249)
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var bubbleWidthAnchor: NSLayoutConstraint?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        addSubview(bubbleView)
         addSubview(textView)
         
         
-        textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleWidthAnchor = bubbleView.widthAnchor.constraint(equalToConstant: 200)
+        bubbleWidthAnchor?.isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        
+        //textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        textView.leftAnchor.constraint(equalTo: bubbleView.leftAnchor, constant: 8).isActive = true
         textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        textView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        textView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        textView.rightAnchor.constraint(equalTo: bubbleView.rightAnchor).isActive = true
         textView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
     }
     
